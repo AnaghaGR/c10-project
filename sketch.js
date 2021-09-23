900cce344aac0762695a7afe36f0b7f54a3574fc
AnaghaGR <87880206+AnaghaGR@users.noreply.github.com>
2021-09-23 16:34:15 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,19 +8,19 @@ ship_moving=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
 
 function setup(){
   createCanvas(400,400);
-  sea=createSprite(200,200,20,20)
+  sea=createSprite(400,200)
   sea.addImage(seaImage)
-  ship=createSprite(200,170,20,20)
+  ship=createSprite(200,220,20,20)
   ship.addAnimation("moving",ship_moving)
   ship.scale=0.1
-  sea.scale=0.1
+  sea.scale=0.3
 }
 
 function draw(){
-background("blue")
-sea.velocityX=5;
+background(0) 
+sea.velocityX=-5;
 if(sea.x<0){
-sea.x=sea.width/2
+sea.x=sea.width/8
 }
 drawSprites()
 }
